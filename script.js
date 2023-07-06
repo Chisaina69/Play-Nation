59fe5c60e873694187027c2561a8f4cfbdd3dd40
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-07 02:36:53 +0300

    added unction to create game cards

--- script.js
+++ script.js
@@ -33,3 +33,46 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   }
+
+  // Function to create a game card
+  function createGameCard(game) {
+    const gameCard = document.createElement("div");
+    gameCard.className = "game-card";
+    gameCard.dataset.url = game.game_url;
+
+    const gameImage = document.createElement("img");
+    gameImage.className = "game-image";
+    gameImage.src = game.thumbnail;
+    gameImage.alt = game.title;
+    gameCard.appendChild(gameImage);
+
+    const gameDetails = document.createElement("div");
+    gameDetails.className = "game-details";
+
+    const gameTitle = document.createElement("h2");
+    gameTitle.className = "game-title";
+    gameTitle.textContent = game.title;
+    gameDetails.appendChild(gameTitle);
+
+    const gameDescription = document.createElement("p");
+    gameDescription.className = "game-description";
+    gameDescription.textContent = game.short_description;
+    gameDetails.appendChild(gameDescription);
+
+    const gameCategory = document.createElement("p");
+    gameCategory.className = "game-category";
+    gameCategory.textContent = `Category: ${game.genre}`;
+    gameDetails.appendChild(gameCategory);
+
+    if (game.release_date === "TBD") {
+      const releaseSoon = document.createElement("span");
+      releaseSoon.className = "release-soon";
+      releaseSoon.textContent = "Release Soon";
+      gameDetails.appendChild(releaseSoon);
+    }
+
+    gameCard.appendChild(gameDetails);
+
+    return gameCard;
+  }
+});
