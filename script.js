bd28f6c1708f7ac1ddf67387d0c5943f87f18b2d
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-06 23:41:47 +0300

    fetched data

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gameContainer = document.getElementById("game");
+  const errorMessage = document.createElement("p");
+  errorMessage.className = "error-message";
+  errorMessage.textContent =
+    "Error fetching game data. Please try again later.";
 
+  fetch("https://www.freetogame.com/api/games")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((game) => {
+        const gameCard = createGameCard(game);
+        gameContainer.appendChild(gameCard);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      gameContainer.appendChild(errorMessage);
+    });
+
+  gameContainer.addEventListener("click", (event) => {
+    const clickedGameCard = event.target.closest(".game-card");
+    if (clickedGameCard) {
+      const gameURL = clickedGameCard.dataset.url;
+      if (gameURL) {
+        window.location.href = gameURL;
+      }
+    }
+  });
+});
